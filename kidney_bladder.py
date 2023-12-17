28a0dcbc5c3d9d45d565bf01bd16cfb6f09c9367
Calvin Cai <calvincai@Calvins-MacBook-Pro.local>
2023-12-17 12:04:11 -0500

    cleaned up merge table, changed input data

--- kidney_bladder.py
+++ kidney_bladder.py
@@ -11,6 +11,18 @@ location1 = 'Colon'
 location2 = 'Rectum'
 
 
+""" Helper Methods """
+
+def to_float(fraction: str):
+    if isinstance(fraction, str):
+        a, b = fraction.replace(',', '').split('/')
+        return round(float(a) / float(b), 4)
+    else:
+        return fraction
+
+
+'''------------------------------------------------------------------------------------'''
+
 '''
     Join data from two dataframes into one easy-to-read dataframe
     Parameters: 2 dataframes
@@ -18,12 +30,17 @@ location2 = 'Rectum'
 def merge_data(df_1, df_2):
     combined_data = pd.merge(df_1, df_2, on='DNA Change', how='outer')
     combined_data.drop(['Impact_x'], axis=1, inplace=True)  
-    combined_data.rename(columns={'# Affected Cases in Cohort_x': f'{location1} Frequency', '# Affected Cases in Cohort_y': f'{location2} Frequency', 'Impact_y': 'Impact'}, inplace=True)
-    combined_data[['Fraction', f'{location1} Percentage']] = combined_data[f'{location1} Frequency'].str.split(',', expand=True)
-    combined_data[['Fraction2', f'{location2} Percentage']] = combined_data[f'{location2} Frequency'].str.split(',', expand=True)
-    combined_data.drop([f'{location1} Frequency', f'{location2} Frequency', 'Fraction', 'Fraction2'], axis=1, inplace=True)
+    combined_data.rename(columns={'# Affected Cases in Cohort_x': f'{location1} Frequency', '# Affected Cases in Cohort_y': f'{location2} Frequency', 'Impact_y': 'Impact', '# Affected Cases Across the GDC_x' : 'Total'}, inplace=True)
+    combined_data[['Fraction', f'{location1} Cases']] = combined_data[f'{location1} Frequency'].str.split(',', expand=True)
+    combined_data[['Fraction2', f'{location2} Cases']] = combined_data[f'{location2} Frequency'].str.split(',', expand=True)
+    combined_data['Total'] = combined_data['Total'].combine_first(combined_data['# Affected Cases Across the GDC_y'])
+    combined_data['Total Cases'] = combined_data['Total'].apply(lambda x: to_float(x))
+    combined_data['Colon Cases'] = combined_data['Colon Cases'].str.rstrip('%').astype('float') / 100.0
+    combined_data['Rectum Cases'] = combined_data['Rectum Cases'].str.rstrip('%').astype('float') / 100.0
+    combined_data.drop([f'{location1} Frequency', f'{location2} Frequency', 'Fraction', 'Fraction2', '# Affected Cases Across the GDC_y', 'Impact'], axis=1, inplace=True)
     return combined_data
 
+'''------------------------------------------------------------------------------------'''
 
 '''
     Finds mutations that are in one form of cancer but not the other
@@ -37,12 +54,15 @@ def find_differences(df):
 
     return ([data1_mutations['DNA Change'], data2_mutations['DNA Change']])
 
+'''------------------------------------------------------------------------------------'''
+
 '''
     Find number of mutations in only one of the cancer forms
 '''
 def find_num_differences(df):
     return f"{location1}: {len(find_differences(df)[0])}\n{location2}: {len(find_differences(df)[1])}"
 
+'''------------------------------------------------------------------------------------'''
 
 '''
     Find common mutations in two cancer forms
@@ -52,6 +72,7 @@ def find_commonalities(df_1, df_2):
     joined_data.rename(columns={'# Affected Cases in Cohort_x': f'{location1} Frequency', '# Affected Cases in Cohort_y': f'{location2} Frequency', 'Impact_y': 'Impact'}, inplace=True)
     return joined_data
 
+'''------------------------------------------------------------------------------------'''
 
 '''
     Find number of common mutations in two cancer forms
@@ -59,8 +80,11 @@ def find_commonalities(df_1, df_2):
 def find_num_commonalities(df_1, df_2):
     return len(find_commonalities(df_1, df_2))
 
+'''------------------------------------------------------------------------------------'''
+
 
 #Merged Colon and Rectum Data
 merged_data = merge_data(colon_data, rectum_data)
 
 print(merged_data)
+
